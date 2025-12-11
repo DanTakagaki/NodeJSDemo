b701a98777c45fbead1a78dd30b83bdc34bfd5d8
Daniel Takagaki <daniel.antoriano@gm2dev.com>
2025-12-11 17:24:51 -0300

    chore: Added delete  product using Mongoose

--- basics-mongodb/controllers/admin.js
+++ basics-mongodb/controllers/admin.js
@@ -109,7 +109,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId)
+    Product.findByIdAndDelete(prodId)//Mongoose method
         .then(() => {
             console.log('DELETED PRODUCT');
             res.redirect('/admin/products');
